a26004dc2ba195638c4af03d08ccd070d1d8823f
Subramanyarao11 <subramanya11rao@gmail.com>
2022-12-23 21:42:52 +0530

    test

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 const express = require('express');
 const mongoose = require('mongoose');
-const url = process.env.URL;
+const url = process.env.MONGODB_URI;
 const getCourses = require('./routes/course');
 const app = express();
 const port = process.env.PORT || 3000
